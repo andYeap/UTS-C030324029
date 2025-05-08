09e0802d622de5370cf1657d3d06bf1ef35c5dea
Muhammad Aqshal Habibi <aqshalh13@gmail.com>
2025-05-08 19:51:23 +0700

    Menulis code untuk menampilkan data

--- menu.cpp
+++ menu.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <conio.h>
+#include <vector>
 using namespace std;
 
 
 void dMenu(){
-system("cls");
-cout<<"Aplikasi Sorting Bubble"<<"\n";       
-cout<<"1. Memasukkan data"<<"\n";            
-cout<<"2. Menampilkan data"<<"\n";            
-cout<<"3. Sorting"<<"\n";           
-cout<<"4. Sepatah kata"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+  system("cls");
+  cout<<"Sort Insertion"<<"\n";       
+  cout<<"1. Memasukkan data"<<"\n";            
+  cout<<"2. Menampilkan data"<<"\n";            
+  cout<<"3. Sorting"<<"\n";           
+  cout<<"4. Sepatah kata"<<"\n";            
+  cout<<"5. Exit"<<"\n";           
+  cout<<"Masukan angka :";        
 }
 
 void mPertama(string pesan){
@@ -21,14 +21,37 @@ cout<<"hallo saya menu "<<pesan;
 getch();
 }
 
-void sepatahKata(){
+void insertionSort(int data[], int n) {
   system("cls");
-  cout <<"Indonesia Bisa, Kita Juara\n @poliban";
+  int temp, j;
+  for (int i = 1; i < n; i++) {
+    temp = data[i];
+    j = i - 1;
+    while (j >= 0 && data[j] > temp) {
+      data[j + 1] = data[j];
+      j--;
+    }
+    data[j + 1] = temp;
+  }
+  
+  for (int i = 0; i < n; i++)
+  {
+    cout << data[i] << " ";
+  }
+  
+  getch();
+}
+
+void sepatahKata() {
+  system("cls");
+  cout << "Halo dunia" << endl;
   getch();
 }
 
 int main() {
 char pl;
+int data[10] = {5, 2, 9, 1, 5, 6, 3, 7, 8, 4};
+
 do
 {
     dMenu();
@@ -40,7 +63,8 @@ do
     mPertama("pertama");
     break;
    case '2':
-    mPertama("ke- dua");
+    // mPertama("ke- dua");
+    insertionSort(data, sizeof(data) / sizeof(data[0]));
     /* code */ 
     break;  
    case '3':
